6f80fb8a74b3c831a5c96ea8b3b5fc07a2f42896
laparici9 <laparici9@gmail.com>
2022-11-02 15:03:52 -0500

    Se corrigió la asociación de clases en el archivo usuarios_cuentas_bancarias y se agregó la asignatura de práctica modular

--- usuarios_cuentas_bancarias.py
+++ usuarios_cuentas_bancarias.py
@@ -1,38 +1,3 @@
-import usuarios_cuentas_bancarias_bonus2 as CuentaBancaria
-
-class Usuario:
-
-    def __init__(self, nombre, cuenta):
-        self.nombre = nombre
-        # self.tipo_cuenta = tipo_cuenta
-        self.cuenta = cuenta
-    
-    def hacer_deposito(self, monto):
-        self.cuenta.deposito += monto
-    
-    def hacer_retiro(self, monto):
-        self.cuenta.retiro -= monto
-
-    def mostrar_balance(self):
-        print(f"la persona {self.nombre} tiene un {self.cuenta.mostrar_info_cuenta}")
-
-    def transferir_dinero(self, otro_usuario, monto):
-        if type(otro_usuario) is not Usuario:
-            print("El usuario no es válido")
-            return
-        if self.cuenta.mostrar_info_cuenta < monto:
-            print("Saldo insuficiente")
-            return
-        self.hacer_retiro(monto) 
-        otro_usuario.hacer_deposito(monto)
-
-cuenta = CuentaBancaria(1000, 0.01)
-print (cuenta.deposito(100))
-# miguel = Usuario("miguel", CuentaBancaria(balance=0, tasa_interes=0.02))
-# matias = Usuario("matias")
-# marcela = Usuario("marcela")
-
-
 class CuentaBancaria:
 
     def __init__(self, balance, tasa_interes):
@@ -48,15 +13,52 @@ class CuentaBancaria:
             self.balance -= 5
             return print("Fondos insuficientes: cobrando una tarifa de $5")
         self.balance -= monto
+        return self.balance
     
     def mostrar_info_cuenta(self):
-        print(f"Balance: ${self.balance}")
+        return f"Balance: ${self.balance}"
 
-    def generar_interés(self): 
+    def generar_interes(self): 
         if self.balance > 0:
             self.balance += (self.balance * self.tasa_interes)
 
 
+class Usuario:
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.cuenta = CuentaBancaria(balance=0, tasa_interes=0.02)
+    
+    def hacer_deposito(self, monto):
+        self.cuenta.deposito(monto)
+        return self
+    
+    def hacer_retiro(self, monto):
+        self.cuenta.retiro(monto)
+        return self
+
+    def mostrar_balance(self):
+        print(f"la persona {self.nombre} tiene un {self.cuenta.mostrar_info_cuenta()}")
+
+    def transferir_dinero(self, otro_usuario, monto):
+        if type(otro_usuario) is not Usuario:
+            print("El usuario no es válido")
+            return
+        if self.cuenta.mostrar_info_cuenta < monto:
+            print("Saldo insuficiente")
+            return
+        self.hacer_retiro(monto) 
+        otro_usuario.hacer_deposito(monto)
+
+
+miguel = Usuario("miguel")
+miguel.hacer_deposito(1000).hacer_deposito(200).hacer_retiro(100).mostrar_balance()
+
+matias = Usuario("matias")
+matias.hacer_deposito(2000).hacer_deposito(100).hacer_retiro(100).mostrar_balance()
+
+marcela = Usuario("marcela")
+marcela.hacer_deposito(500).hacer_deposito(700).hacer_retiro(200).mostrar_balance()
 
 # cuenta1 = CuentaBancaria(1000, 0.01)
 # cuenta2 = CuentaBancaria(500, 0.01)
